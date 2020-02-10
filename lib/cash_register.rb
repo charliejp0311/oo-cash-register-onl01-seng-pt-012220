e809d5d148cc6ab50e770297979322d3a2c3b30c
Charles Pelton <charliejp0311@gmail.com>
2020-02-10 17:11:29 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,10 +1,11 @@
 require "pry"
 class CashRegister
-  attr_accessor :cash_register, :cash_register_with_discount
+  attr_accessor :cash_register, :cash_register_with_discount, :employee_discount
   attr_reader :total
 
-  def initialize(discount = 0)
+  def initialize(discount = 0, emp_dis = 0)
     @cash_register_with_discount = discount
+    @employee_discount = emp_dis
     @total = 0
   end
 
