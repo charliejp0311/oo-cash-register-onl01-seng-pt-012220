090b4e25d0397a6ac98931bde9ba9bbb06e4f6a6
Charles Pelton <charliejp0311@gmail.com>
2020-02-10 17:01:25 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,9 +1,11 @@
+require "pry"
 class CashRegister
   attr_accessor :cash_register, :cash_register_with_discount
   attr_reader :total
 
-  def initialize
+  def initialize(idk_what_this_is_yet)
     @total = 0
+    binding.pry
   end
 
 
