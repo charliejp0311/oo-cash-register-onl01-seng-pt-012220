f4e2975fd7e4e6c8cfee8da25abbf79a52f89f9e
Charles Pelton <charliejp0311@gmail.com>
2020-02-10 17:37:20 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -3,11 +3,13 @@ class CashRegister
   attr_accessor :cash_register, :cash_register_with_discount
   attr_reader :total
 
-  def initialize(discount = 0)
-    @cash_register_with_discount = discount
+  def initialize(disc = 0)
+    @cash_register_with_discount = disc
     @total = 0
     binding.pry
   end
+  def discount
 
+  end
 
 end
