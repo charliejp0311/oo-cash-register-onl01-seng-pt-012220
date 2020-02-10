72744dcf2c445ac783f7fe5672ebe4e7474c788a
Charles Pelton <charliejp0311@gmail.com>
2020-02-10 17:41:48 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,15 +1,17 @@
 require "pry"
 class CashRegister
-  attr_accessor :cash_register, :cash_register_with_discount
-  attr_reader :total
+  attr_accessor :cash_register, :cash_register_with_discount, :total
 
   def initialize(disc = 0)
     @cash_register_with_discount = disc
     @total = 0
-    binding.pry
   end
+  
   def discount
     @cash_register_with_discount = 20
   end
-
+  
+  def total
+    @total
+  end
 end
