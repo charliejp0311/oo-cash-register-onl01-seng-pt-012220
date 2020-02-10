85a65f62aa37b6807bf30273981c17d9c6c49742
Charles Pelton <charliejp0311@gmail.com>
2020-02-10 17:16:00 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -5,8 +5,8 @@ class CashRegister
 
   def initialize(discount = 0, emp_dis = false)
     @cash_register_with_discount = discount
-    emp_dis ? @cash_register_with_discount = 20 : @cash_register_with_discount = 0 
-    
+    emp_dis ? @cash_register_with_discount = 20 : @cash_register_with_discount = 0
+
     @total = 0
   end
 
